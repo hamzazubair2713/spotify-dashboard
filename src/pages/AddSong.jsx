9438dd25eb4c55673ad306c82035b43bce0bd3a8
Hamza Zubair <hamza@webevis.com>
2024-09-16 17:29:59 +0500

    implement add song api

--- src/pages/AddSong.jsx
+++ src/pages/AddSong.jsx
@@ -1,88 +1,146 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
 import { assets } from "../assets/assets";
+import axios from "axios";
 
 const AddSong = () => {
+  const url = "http://localhost:4009";
   const [formData, setFormData] = useState({
-    image: false,
     song: false,
+    image: false,
     name: "",
     desc: "",
     album: "none",
   });
-  const [albumData, setAlbumData] = useState([]);
+  const [albumData, setAlbumData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const convertToFormData = (obj) => {
+    const formData = new FormData();
+
+    Object.keys(obj).forEach((key) => {
+      formData.append(key, obj[key]);
+    });
+    return formData;
+  };
   async function handelSubmit(e) {
     e.preventDefault();
-    let form = e.target;
-    let formData = new FormData(form);
-    let formObj = Object.fromEntries(formData.entries());
-    console.log(formObj);
+    const data = new FormData();
+    data.append("name", formData.name);
+    data.append("desc", formData.desc);
+    data.append("image", formData.image);
+    data.append("audio", formData.song);
+    data.append("album", formData.album);
+    // const data = convertToFormData(formData);
+    const resp = await axios.post(`${url}/api/song/add`, data);
+    console.log(resp);
   }
   return (
-    <form
-      className="flex flex-col items-start gap-8 text-gray-600"
-      onSubmit={handelSubmit}
-    >
-      <div className="flex gap-8">
-        <div className="flex flex-col gap-4">
-          <p>Upoad Song</p>
-          <input type="file" id="song" name="song" accept="audio/*" hidden />
-          <label htmlFor="song">
-            <img
-              src={assets.upload_song}
-              className="w-24 cursor-pointer"
+    <>
+      {loading ? (
+        <div className="grid place-items-center min-h-[80vh]">
+          <div className="w-16 h-16 place-self-center border-4 border-gray-400 border-t-green-800 rounded-full animate-spin " />
+        </div>
+      ) : (
+        <form
+          className="flex flex-col items-start gap-8 text-gray-600"
+          onSubmit={handelSubmit}
+        >
+          <div className="flex gap-8">
+            <div className="flex flex-col gap-4">
+              <p>Upoad Song</p>
+              <input
+                type="file"
+                id="song"
+                name="song"
+                accept="audio/*"
+                hidden
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    song: e.target.files[0],
+                  }))
+                }
+              />
+              <label htmlFor="song">
+                <img
+                  src={formData.song ? assets.upload_added : assets.upload_song}
+                  className="w-24 cursor-pointer"
+                />
+              </label>
+            </div>
+            <div className="flex flex-col gap-4">
+              <p>Upload Image</p>
+              <input
+                type="file"
+                id="image"
+                name="image"
+                accept="image/*"
+                hidden
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, image: e.target.files[0] }))
+                }
+              />
+              <label htmlFor="image">
+                <img
+                  src={
+                    formData.image
+                      ? URL.createObjectURL(formData.image)
+                      : assets.upload_area
+                  }
+                  className="w-24 cursor-pointer"
+                  alt=""
+                />
+              </label>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2.5">
+            <p>Song Name</p>
+            <input
+              type="text"
+              className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)] "
+              placeholder="Type Here..."
+              required
+              name="song_name"
+              value={formData.name}
               onChange={(e) =>
-                setFormData((prev) => ({
-                  ...prev,
-                  image: URL.createObjectURL(e.target.file[0]),
-                }))
+                setFormData((prev) => ({ ...prev, name: e.target.value }))
               }
             />
-          </label>
-        </div>
-        <div className="flex flex-col gap-4">
-          <p>Upload Image</p>
-          <input type="file" id="image" accept="image/*" hidden />
-          <label htmlFor="image">
-            <img
-              src={assets.upload_area}
-              className="w-24 cursor-pointer"
-              alt=""
+          </div>
+          <div className="flex flex-col gap-2.5">
+            <p>Song Description</p>
+            <input
+              type="text"
+              className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)] "
+              placeholder="Type Here..."
+              required
+              name="desc"
+              value={formData.desc}
+              onChange={(e) =>
+                setFormData((prev) => ({ ...prev, desc: e.target.value }))
+              }
             />
-          </label>
-        </div>
-      </div>
-      <div className="flex flex-col gap-2.5">
-        <p>Song Name</p>
-        <input
-          type="text"
-          className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)] "
-          placeholder="Type Here..."
-          required
-        />
-      </div>
-      <div className="flex flex-col gap-2.5">
-        <p>Song Description</p>
-        <input
-          type="text"
-          className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)] "
-          placeholder="Type Here..."
-          required
-        />
-      </div>
-      <div className="flex flex-col gap-2.5">
-        <p>Album</p>
-        <select className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[150px]">
-          <option value="none">None</option>
-        </select>
-      </div>
-      <button
-        type="submit"
-        className="text-base bg-black text-white py-2.5 px-14 cursor-pointer"
-      >
-        Add Song
-      </button>
-    </form>
+          </div>
+          <div className="flex flex-col gap-2.5">
+            <p>Album</p>
+            <select
+              className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[150px]"
+              onChange={(e) =>
+                setFormData((prev) => ({ ...prev, album: e.target.value }))
+              }
+            >
+              <option value="none">None</option>
+            </select>
+          </div>
+          <button
+            type="submit"
+            className="text-base bg-black text-white py-2.5 px-14 cursor-pointer"
+          >
+            Add Song
+          </button>
+        </form>
+      )}
+    </>
   );
 };
 
